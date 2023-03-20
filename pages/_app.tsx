db102fd49a1fd32da64ffccdbbe309c6f4b416c4
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-20 17:30:52 +0100

    Upload skillshare

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
+import { lightTheme } from '@rainbow-me/rainbowkit';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
@@ -32,7 +34,13 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider coolMode chains={chains} theme={lightTheme({
+      accentColor: 'linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)',
+      accentColorForeground: 'white',
+      borderRadius: 'small',
+      fontStack: 'system',
+      overlayBlur: 'small',
+    })}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
