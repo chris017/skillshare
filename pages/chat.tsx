c32a99ae885fe9f9e919e70e5bd322de39e516b3
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-21 19:23:56 +0100

    s

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,18 +1,35 @@
 import styles from '../styles/Home.module.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Field, Form, Formik } from 'formik';
 import { useContractWrite, usePrepareContractWrite, useContractRead } from 'wagmi'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import datajs from '../data.json';
+import * as React from 'react';
+export { React };
+
 import {
   Heading,
   Button,
-  Box,
   Container,
   Flex,
   Spacer,
   Text,
   Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Box,
+  ModalBody,
+  ModalCloseButton,
   FormControl,
   FormLabel,
   FormErrorMessage,
@@ -20,31 +37,31 @@ import {
 
 
 function ChatApp() {
-    const config = usePrepareContractWrite({
+  const config = usePrepareContractWrite({
     address: '0x22d819FA52ffDB2465adcfC9B638f925c869f17f',
     abi: datajs.abi,
     functionName: 'createAccount',
     args: ['Chris'],
-    }).config;
-  const { data, isLoading, isSuccess, write } = useContractWrite(config);
-  const [name, setName] = useState('');
-  const [users, setUsers] = useState([]);
+  }).config;
+
   const { data: AllUsers } = useContractRead({
     address: '0x22d819FA52ffDB2465adcfC9B638f925c869f17f',
     abi: datajs.abi,
     functionName: 'getAllAppUser',
   })
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      if (Array.isArray(AllUsers)) {
-        setUsers(AllUsers);
-      }
-    };
-
-    fetchUsers();
-  }, [AllUsers]);
   
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
+  const [name, setName] = useState('');
+  const [showUsers, setShowUsers] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
+  
+
+  const handleShowUsers = () => {
+    setShowUsers(true);
+  };
+
   return (
     <div>
       <Flex>
@@ -69,19 +86,57 @@ function ChatApp() {
         <Heading size="2xl" m={5}>
           Skill<span className={styles.gradient}>-Share</span> Chat
         </Heading>
-        <button disabled={!write} onClick={() => write?.()}>
-            Register
-        </button>
+            <Button 
+            size='md'
+            color="white"
+            bg='green.400'
+            _hover={{ bg: "white", color: "green.400" }}
+            onClick={onOpen}>Register</Button>
+            <Modal
+                initialFocusRef={initialRef}
+                isOpen={isOpen}
+                onClose={onClose}
+                >
+                <ModalOverlay />
+                <ModalContent bg={"#09203f"}>
+                    <ModalHeader>Create your account</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                    <FormControl>
+                    <FormLabel>Create Username</FormLabel>
+                    <Input ref={initialRef} placeholder='username' _active={{borderColor: 'white', }}/>
+                    </FormControl>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button color="white" 
+                    bg='green.400'
+                    _hover={{ bg: "white", color: "green.400" }} mr={3} disabled={!write} onClick={() => write?.()}>
+                    Save
+                    </Button>
+                    <Button onClick={onClose}
+                    color="white"
+                    bg='red.400'
+                    _hover={{ bg: "white", color: "green.400" }}
+                    >Cancel</Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
         {isLoading && <div>Check Wallet</div>}
         {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
       </Container>
-      <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <p>Name: {user.name}</p>
-        </div>
-      ))}
-    </div>
+      <Container>
+        <Box>
+            <Button onClick={handleShowUsers}>Show AllUsers</Button>
+            {showUsers && Array.isArray(AllUsers) && (
+            <ul>
+                {AllUsers.map((user, index) => (
+                <li key={index}>{user}</li>
+                ))}
+            </ul>
+            )}
+        </Box>
+      </Container>
     </div>
   );
 };
