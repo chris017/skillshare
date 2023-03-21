b02f24bc78b093657c414be2c55cebf49a3acd96
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-21 02:48:25 +0100

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from 'next/app';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { arbitrum, goerli, mainnet, optimism, polygon, polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
-
+import { ChakraProvider, CSSReset, theme } from '@chakra-ui/react'
 
 
 const { chains, provider, webSocketProvider } = configureChains(
@@ -35,17 +35,19 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider coolMode chains={chains} theme={lightTheme({
-      accentColor: 'linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)',
-      accentColorForeground: 'white',
-      borderRadius: 'small',
-      fontStack: 'system',
-      overlayBlur: 'small',
-    })}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider coolMode chains={chains} theme={lightTheme({
+            accentColor: 'linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)',
+            accentColorForeground: 'white',
+            borderRadius: 'small',
+            fontStack: 'system',
+            overlayBlur: 'small',})}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+    </ChakraProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,48 +2,66 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { UseContractConfig } from 'wagmi';
+import { useContractRead, UseContractConfig } from 'wagmi';
 import config from '../data.json';
-import { useContractRead } from 'wagmi'
-import { useEffect } from 'react';
-
+import { Heading, Text, Button, ButtonGroup, Box, Flex, Spacer } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   const { data, isError, isLoading } = useContractRead({
-    address: "0x22d819FA52ffDB2465adcfC9B638f925c869f17f",
+    address: '0x22d819FA52ffDB2465adcfC9B638f925c869f17f',
     abi: config.abi,
     functionName: 'getAllAppUser',
   })
-
-  useEffect(()=>{
-    console.log(data);
-  },);
+  if (Array.isArray(data)) {
+    console.log(data[0].name)
+  }
   return (
-    <div className={styles.container}>
+    <Box h='calc(100vh)'>
       <Head>
         <title>Skill-Share</title>
-        <meta
-          content=""
-          name=""
-        />
+        <meta content="" name="" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="">Skill<span>-Share</span></a>
-        </h1>
-        <p className={styles.description}>
-          Skill<span>-Share</span> is a decentralized application that allows users to<br></br>exchange skills with each other in a <span>peer-to-peer</span> manner.<br></br>The dApp can be used to <span>connect</span> users with complementary<br></br>skills and facilitate skill<span>-sharing</span> and learning.
-        </p>
-        <ConnectButton accountStatus={{
-          smallScreen: 'avatar',
-          largeScreen: 'full',
-          }}
-          showBalance={{
-            smallScreen: false,
-            largeScreen: true,}}/>
+      <main>
+        <Flex>
+          <Box m='5'>
+            <Heading size={'lg'}>Skill<span className={styles.gradient}>-Share</span></Heading>
+          </Box>
+          <Spacer />
+          <Box m='5'>
+            <ConnectButton
+              accountStatus={{
+                smallScreen: 'avatar',
+                largeScreen: 'full',
+              }}
+              showBalance={{
+                smallScreen: false,
+                largeScreen: true,
+              }}
+            />
+          </Box>
+        </Flex>
+        <Box className={styles.main} pt={20}>
+          <Heading size="3xl" m={5}> 
+            Welcome to Skill<span className={styles.gradient}>-Share</span>
+          </Heading>
+          <Heading size="lg" mb={10}>
+            Skill<span className={styles.gradient}>-Share</span> is a decentralized application that allows users to<br></br>exchange skills with each other in a <span className={styles.gradient}>peer-to-peer</span> manner.<br></br>The dApp can be used to <span className={styles.gradient}>connect</span> users with complementary<br></br>skills and facilitate skill<span className={styles.gradient}>-sharing</span> and learning.
+          </Heading>
+          <Button 
+            size='md'
+            height='48px'
+            width='200px'
+            border='2px'
+            borderColor='green.400'
+            bg='linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)'
+            _hover={{ bg: "linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)" }}
+            >
+            Get Started
+          </Button>
+        </Box>
       </main>
-    </div>
+    </Box>
   );
 };
 
