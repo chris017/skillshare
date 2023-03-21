80728725729253e5130a70af3acea7d7d5d645ed
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-21 03:59:12 +0100

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,33 @@ const Home: NextPage = () => {
         <Center height='50px' m={5}>
           <Divider orientation='vertical' />
         </Center>
-        
+        <List spacing={3}>
+          <ListItem>
+            <Text size={"xl"}><ListIcon as={CheckIcon} color='green.500' />
+              User Authentication: Users can sign in to the dApp using their preferred method of authentication such as MetaMask, WalletConnect, or other Web3 wallets.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text size={"xl"}><ListIcon as={CheckIcon} color='green.500' />
+              Skill Listing: Users can list the skills they have to offer and the skills they want to learn on the dApp.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text size={"xl"}><ListIcon as={CheckIcon} color='green.500' />
+              Skill Matching: The dApp can use algorithms to match users with complementary skills and facilitate skill-sharing and learning.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text size={"xl"}><ListIcon as={CheckIcon} color='green.500' />
+              Chat System: The dApp can have a built-in chat system that allows users to communicate and arrange skill-sharing sessions.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text size={"xl"}><ListIcon as={CheckIcon} color='green.500' />
+              Rating and Review System: The dApp can have a rating and review system that allows users to rate and review each other's skills and facilitate trust between users.
+            </Text>
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
