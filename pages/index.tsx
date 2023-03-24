ff0590b850525df101a98acb994ed5488182745e
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-24 21:21:55 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,7 +105,7 @@ const Home: NextPage = () => {
                 &gt; Person B: That sounds really cool. How does it work?
               </Container>
               <Container bg={"white"} borderRadius={"md"} fontSize={"xl"}>
-                &gt; Person A: Users can join different groups based on their interests, and then they can chat, share resources, and help each other out. Skillshare on Polygon provides a safe and supportive environment to learn new things and connect with like-minded individuals.
+                &gt; Person A: Users can chat with other Persons based on their interests, and can share resources, and help each other out. Skillshare on Polygon provides a safe and supportive environment to learn new things and connect with like-minded individuals.
               </Container>
             </VStack>
           </Box>
@@ -120,22 +120,7 @@ const Home: NextPage = () => {
             </ListItem>
             <ListItem>
               <Text fontSize={"lg"}><ListIcon as={CheckIcon} color='green.500' />
-                Skill Listing: Users can list the skills they have to offer and the skills they want to learn on the dApp.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text fontSize={"lg"}><ListIcon as={CheckIcon} color='green.500' />
-                Skill Matching: The dApp can use algorithms to match users with complementary skills and facilitate skill-sharing and learning.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text fontSize={"lg"}><ListIcon as={CheckIcon} color='green.500' />
-                Chat System: The dApp can have a built-in chat system that allows users to communicate and arrange skill-sharing sessions.
-              </Text>
-            </ListItem>
-            <ListItem>
-              <Text fontSize={"lg"}><ListIcon as={CheckIcon} color='green.500' />
-                Rating and Review System: The dApp can have a rating and review system that allows users to rate and review each other's skills and facilitate trust between users.
+                Chat System: The dApp has a built-in chat system that allows users to communicate and arrange skill-sharing sessions.
               </Text>
             </ListItem>
           </List>
