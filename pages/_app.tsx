b64a86c9666ac1065cdb64bd51d349c07d80d1ff
Christian Schmid <chrisi.schmid112@gmail.com>
2023-03-24 15:04:36 +0100

    support for testchain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,30 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { lightTheme } from '@rainbow-me/rainbowkit';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { arbitrum, goerli, mainnet, optimism, polygon, polygonMumbai } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { ChakraProvider, CSSReset, theme } from '@chakra-ui/react'
-
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { lightTheme } from "@rainbow-me/rainbowkit";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  arbitrum,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  polygonMumbai,
+} from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    polygon,
     polygonMumbai,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: "RainbowKit App",
   chains,
 });
 
@@ -34,16 +39,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider coolMode modalSize="compact" chains={chains} theme={lightTheme({
-            accentColor: 'linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)',
-            accentColorForeground: 'white',
-            borderRadius: 'small',
-            fontStack: 'system',
-            overlayBlur: 'small',})}>
-            <Component {...pageProps} />
-          </RainbowKitProvider>
-        </WagmiConfig>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          coolMode
+          modalSize="compact"
+          chains={chains}
+          theme={lightTheme({
+            accentColor:
+              "linear-gradient(140deg, rgb(127, 198, 179) 0%, rgb(48, 245, 90) 100%)",
+            accentColorForeground: "white",
+            borderRadius: "small",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ChakraProvider>
   );
 }
